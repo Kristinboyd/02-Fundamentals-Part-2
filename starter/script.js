5ba44692ce619bb0ea86cf0fc0f2a35107a0900b
Kristin <Kristineliseboyd13@gmail.com>
2021-11-02 16:12:56 -0700

    completes functions

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,32 @@
 'use strict';
 
-// Activating Strict Mode 
+// Activating Strict Mode
 // ****************************************************************
 // by typing 'use strict'; at the top of each file
+// if there is a typo in the below, it will return a error in the
+// console. If there is no error, it will read as normal
+// let hasDriversLicense = false;
+// const passTest = true;
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log(`I can drive`);
 
-let hasDriversLicense = false;
-const passTest = true;
+// Functions
+// *****************************************************************
+function logger() {
+  console.log(`My name is Kristin`);
+}
+// calling / running / invoking the function
+logger();
+logger();
+logger();
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log(`I can drive`);
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges!`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
