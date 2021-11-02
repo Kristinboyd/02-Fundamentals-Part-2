cbd8aaa7ef5cb07f7a33846a52187f74bba96571
Kristin <Kristineliseboyd13@gmail.com>
2021-11-02 15:35:01 -0700

    completes section intro and activates strict mode

---
+++ starter/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Activating Strict Mode 
+// ****************************************************************
+// by typing 'use strict'; at the top of each file
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log(`I can drive`);
