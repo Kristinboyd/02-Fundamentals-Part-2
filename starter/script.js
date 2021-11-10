8cb61143edeccc28bacf65a979ac7a1e254cda9f
Kristin <Kristineliseboyd13@gmail.com>
2021-11-10 10:24:08 -0800

    completes functions review

--- starter/script.js
+++ starter/script.js
@@ -60,16 +60,36 @@
 
 // Functions calling other functions
 // *****************************************************************
-function cutFruitPieces(fruit) {
-  return fruit * 4;
-}
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+//   console.log(apples, oranges);
+//   const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange!`;
+//   return juice;
+// }
+// console.log(fruitProcessor(2, 3));
 
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
-  console.log(apples, oranges);
-  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange!`;
-  return juice;
-}
+// Reviewing Functions
+// ***************************************************************
+// const calcAge = function(birthYear) {
+//   return 2037 - birthYear;
+// };
+// const yearsUntilRetirement = function(birthYear, firstName) {
+//   const age = calcAge(birthYear);
+//   const retirement = 65 - age;
+//   if (retirement > 0) {
+//     console.log(`${firstName} retires in ${retirement} years!`);
+//     return retirement;
+//   } else {
+//     console.log(`${firstName} has already retired!`);
+//     return -1;
+//   }
+// };
+// console.log(yearsUntilRetirement(1992, `Kristin`));
+// console.log(yearsUntilRetirement(1950, `Mike`));
 
-console.log(fruitProcessor(2, 3));
+// Coding Challenge Part Two
+// ***************************************************************
