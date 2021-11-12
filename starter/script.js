4485674032f1ca7141c701e1697c58b081e98a4b
Kristin <Kristineliseboyd13@gmail.com>
2021-11-12 10:29:13 -0800

    completes Coding Challenge #1

--- starter/script.js
+++ starter/script.js
@@ -91,5 +91,32 @@
 // console.log(yearsUntilRetirement(1992, `Kristin`));
 // console.log(yearsUntilRetirement(1950, `Mike`));
 
-// Coding Challenge Part Two
+// Coding Challenge One
 // ***************************************************************
+// const calcAverage = (a, b, c) => (a + b + c) / 3;
+// console.log(calcAverage(3, 4, 5));
+// // Test 1
+// let scoreDolphins = calcAverage(44, 23, 71);
+// let scoreKoalas = calcAverage(65, 54, 49);
+// console.log(scoreDolphins, scoreKoalas);
+// const checkWinner = function(avgDolphins, avgKoalas) {
+//   if (avgDolphins >= 2 * avgKoalas) {
+//     console.log(`Dolphins win! The score was ${avgDolphins} vs ${avgKoalas}`);
+//   } else if (avgKoalas >= 2 * avgDolphins) {
+//     console.log(`Koalas win! The score was ${avgDolphins} vs ${avgKoalas}`);
+//   } else {
+//     console.log(`No team wins....`);
+//   }
+// };
+// checkWinner(scoreDolphins, scoreKoalas);
+// // Test 2
+// scoreDolphins = calcAverage(85, 54, 41);
+// scoreKoalas = calcAverage(23, 34, 27);
+// console.log(scoreDolphins, scoreKoalas);
+// checkWinner(scoreDolphins, scoreKoalas);
+
+// Arrays
+// ***************************************************************
+
+
+
