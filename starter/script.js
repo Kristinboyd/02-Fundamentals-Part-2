a7863e5cc79b32431a569ce1d5c08aaed815108a
Kristin <Kristineliseboyd13@gmail.com>
2021-11-12 10:51:37 -0800

    completes intro to arrays

--- starter/script.js
+++ starter/script.js
@@ -117,6 +117,43 @@
 
 // Arrays
 // ***************************************************************
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
 
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
 
+// other way
+const year = new Array(1992, 1991, 2003, 2010);
+console.log(year);
 
+console.log(friends[0]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]); // gives you the last element in the array
+
+friends[2] = 'Jay';
+console.log(friends); // reassigns Peter with Jay
+// We can always change things in an Array. It is muteable. Although we cannot replace the entire array.
+
+const firstName = 'Kristin';
+const kristin = [firstName, 'Boyd', 2021 - 1992, 'Software Developer', friends];
+console.log(kristin);
+console.log(kristin.length);
+
+const calcAge = function(birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1])
+];
+console.log(ages);
