c105a595850df1bee4afeb79e99334d6fcafce5d
Kristin <Kristineliseboyd13@gmail.com>
2021-11-12 12:24:30 -0800

    completes coding challenge #2

--- starter/script.js
+++ starter/script.js
@@ -188,3 +188,18 @@
 
 // Coding Challenge #2
 // ********************************************************************************
+// function expression
+// const calcTip = function(bill) {
+//   return bill >= 50 && 300 <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+// // arrow function
+// // const calcTip = bill => (bill >= 50 && 300 <= 300 ? bill * 0.15 : bill * 0.2);
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(tips, bills);
+// //
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(totals);
+
+// Intro to Objects 
+// *******************************************************************************
